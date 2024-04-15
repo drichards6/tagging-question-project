51392ba995d00d609070d21b569cc451a9b54c4c
drichards6 <drr5489@psu.edu>
2024-04-14 22:43:10 -0400

    initial boilerplate work

--- src/tagging-question.js
+++ src/tagging-question.js
@@ -9,6 +9,8 @@ export class TaggingQuestion extends DDD {
 
   constructor() {
     super();
+    this.img = "https://lumiere-a.akamaihd.net/v1/images/ct_frozen_elsa_18466_22a50822.jpeg";
+    this.instructions= "let it go !";
   }
   
   static get styles() {
@@ -24,16 +26,41 @@ export class TaggingQuestion extends DDD {
         height: 95vh;
         width:  95vw;
         margin: var(--ddd-spacing-10);
-        text-align: center;
+        
+      }
+
+      .img-container {
+        display: flex;
+        margin-left: auto;
+        margin-right: auto;
+        height: 50vh;
+        width: 50vw;
         justify-content: center;
       }
 
-      img {
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-        width: 50%;
-      }
+    .instruction-container {
+        text-align: center;
+    }
+
+    .check-answers-btn {
+        background-color: var(--ddd-theme-default-potential70);
+        color: var(--ddd-theme-default-slateMaxLight);
+    }
+
+    .check-answers-btn:focus,
+    .check-answers-btn:hover {
+        background-color: var(--ddd-theme-default-creekTeal);
+        color: var(--ddd-theme-default-coalyGray);
+    }
+
+    .option-container {
+        border: 1px solid grey;
+        width: 80vw;
+        overflow: auto;
+        white-space: nowrap;
+        display: flex;
+        flex-direction: row;
+    }
 
     `];
   }
@@ -42,9 +69,21 @@ export class TaggingQuestion extends DDD {
     return html `
         <div class="tagging-question-container">
             <div class="img-container">
-                <img class="img" src="https://lumiere-a.akamaihd.net/v1/images/ct_frozen_elsa_18466_22a50822.jpeg">
-            </div> <!--end img-container-->
-        </div> <!--end tagging-question-container-->
+                <img class="img" src="${this.img}">
+            </div> <!--end img-container div-->
+            <div class="instruction-container">
+                <p><strong>${this.instructions}</strong></p>
+            </div> <!--end instruction-container div-->
+            <div class="option-container">
+                <p>this is where the option bank of tags will be displayed</p>
+            </div> <!--end option-container div-->
+            <div class="answer-container">
+                <p>this is where the answer tags will be displayed</p>
+            </div> <!--end answer-container div-->
+            <div class="check-button-container">
+                <button class="check-answers-btn">Check Answers</button>
+            </div> <!--end check-button-container div-->
+        </div> <!--end tagging-question-container div-->
     `;
   }
 
@@ -52,6 +91,8 @@ export class TaggingQuestion extends DDD {
   static get properties() {
     return {
       ...super.properties,
+      img: { type: String },
+      instructions: { type: String },
     }
   }
 
